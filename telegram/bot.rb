1df6ff5a8dcab838b4f2710a7a731d0cb33de926
Светлана Курилова <svetakurilova2001@gmail.com>
2022-06-15 16:40:40 +0300

    the ability to add a bot to a group

--- telegram/bot.rb
+++ telegram/bot.rb
@@ -13,9 +13,12 @@ Telegram::Bot::Client.run(token) do |bot|
       Message.create(telegram_id: message.from.id, first_name: message.from.first_name)
     end
     message.inspect
-    case message.text
-    when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: "Привет, #{message.from.first_name}")
+    if message.class.to_s == Telegram::Bot::Types::ChatMemberUpdated.to_s
+    else 
+      case message.text
+      when '/start'
+        bot.api.send_message(chat_id: message.chat.id, text: "Привет, #{message.from.first_name}")
+      end
     end
   end
 end
