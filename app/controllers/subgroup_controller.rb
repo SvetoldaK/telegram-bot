3e231e1c3d20e9d2793387b912e00f9d5ff8525d
Светлана Курилова <svetakurilova2001@gmail.com>
2022-06-21 14:38:30 +0300

    deleting subgroups on the site

--- app/controllers/subgroup_controller.rb
+++ app/controllers/subgroup_controller.rb
@@ -1,8 +1,30 @@
 class SubgroupController < ApplicationController
+
   def create
     @subgroup = Subgroup.new(subgroup_params)
     @subgroup.save!
     redirect_to
+  end
+
+  def destroy
+    @subgroup = Subgroup.find(params[:id])
+    @subgroup.destroy
+
+    redirect_to(user_path, status: 303)
+  end
+
+  def edit
+    @subgroup = Subgroup.find(params[:id])
+  end
+
+  def update
+    @subgroup = Subgroup.find(params[:id])
+
+    if @subgroup.update(subgroup_params)
+      redirect_to @user
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
