ff5cb2eab5b5438f8dc4ecef6929dc0887454f1a
Светлана Курилова <svetakurilova2001@gmail.com>
2022-06-28 20:24:21 +0300

    fixing errors when starting the bot

--- telegram/bot.rb
+++ telegram/bot.rb
@@ -8,28 +8,20 @@ token = ENV.fetch('TOKEN', nil)
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
-    # rubocop:disable Lint/EmptyConditionalBody
-    if message.instance_of?(Telegram::Bot::Types::ChatMemberUpdated)
-    elsif message.text.nil?
-    # rubocop:enable Lint/EmptyConditionalBody
-    else
-      # rubocop:disable Style/RegexpLiteral
-      command, text = message.text.match(/\/(\S+)\s*(.*)/)&.captures
-      # rubocop:enable Style/RegexpLiteral
-      chat = message.chat.id
-      # rubocop:disable
-      if message.is_a?(Telegram::Bot::Types::ChatMemberUpdated)
-      # rubocop:enable
-      else
-        case command
-        when 'start'
-          bot.api.send_message(chat_id: message.chat.id,
-                               text:    "Привет, #{message.from.first_name}. Зарегестрируйся на сайте (localhost:3000). Твой id Телеграм чата #{chat}")
-        when command
-          if Group.find_by(name: command, chat_id: Chat.find_by(chat_id: chat).id)
-            bot.api.send_message(chat_id: message.chat.id, text: "#{Group.find_by(name:    command,
-                                                                                  chat_id: Chat.find_by(chat_id: chat).id).nickname} #{text}")
-          end
+    chat = message.chat.id
+
+    # rubocop:disable
+    if message.is_a?(Telegram::Bot::Types::Message)
+    # rubocop:enable
+      command, text = message.text&.match(/\/(\S+)\s*(.*)/)&.captures
+      case command
+      when 'start'
+        bot.api.send_message(chat_id: message.chat.id,
+                            text:    "Привет, #{message.from.first_name}. Зарегестрируйся на сайте (localhost:3000). Твой id Телеграм чата #{message.chat.id}")
+      when command
+        if Group.find_by(name: command, chat_id: Chat.find_by(chat_id: chat)&.id)
+          bot.api.send_message(chat_id: message.chat.id, text: "#{Group.find_by(name:    command,
+                                                                                chat_id: Chat.find_by(chat_id: chat).id).nickname} #{text}")
         end
       end
     end
