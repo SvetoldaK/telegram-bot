4ebfba982d7ede8aeb7226edf41d6ce006a00ef5
Светлана Курилова <svetakurilova2001@gmail.com>
2022-06-28 19:53:54 +0300

    fixing errors when starting the bot

--- telegram/bot.rb
+++ telegram/bot.rb
@@ -8,29 +8,28 @@ token = ENV.fetch('TOKEN', nil)
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
-    if Message.exists?(telegram_id: message.from.id)
-      Message.find_by(telegram_id: message.from.id)
+    # rubocop:disable Lint/EmptyConditionalBody
+    if message.instance_of?(Telegram::Bot::Types::ChatMemberUpdated)
+    elsif message.text.nil?
+    # rubocop:enable Lint/EmptyConditionalBody
     else
-      Message.create(telegram_id: message.from.id, first_name: message.from.first_name)
-    end
-
-    # rubocop:disable Style/RegexpLiteral
-    command, text = message.text.match(/\/(\S+)\s*(.*)/)&.captures
-    # rubocop:enable Style/RegexpLiteral
-    chat = message.chat.id
-
-    # rubocop:disable
-    if message.is_a?(Telegram::Bot::Types::ChatMemberUpdated)
-    # rubocop:enable
-    else
-      case command
-      when 'start'
-        bot.api.send_message(chat_id: message.chat.id,
-                             text:    "Привет, #{message.from.first_name}. Зарегестрируйся на сайте (localhost:3000). Твой id Телеграм чата #{message.chat.id}")
-      when command
-        if Group.find_by(name: command, chat_id: Chat.find_by(chat_id: chat).id)
-          bot.api.send_message(chat_id: message.chat.id, text: "#{Group.find_by(name:    command,
-                                                                                chat_id: Chat.find_by(chat_id: chat).id).nickname} #{text}")
+      # rubocop:disable Style/RegexpLiteral
+      command, text = message.text.match(/\/(\S+)\s*(.*)/)&.captures
+      # rubocop:enable Style/RegexpLiteral
+      chat = message.chat.id
+      # rubocop:disable
+      if message.is_a?(Telegram::Bot::Types::ChatMemberUpdated)
+      # rubocop:enable
+      else
+        case command
+        when 'start'
+          bot.api.send_message(chat_id: message.chat.id,
+                               text:    "Привет, #{message.from.first_name}. Зарегестрируйся на сайте (localhost:3000). Твой id Телеграм чата #{chat}")
+        when command
+          if Group.find_by(name: command, chat_id: Chat.find_by(chat_id: chat).id)
+            bot.api.send_message(chat_id: message.chat.id, text: "#{Group.find_by(name:    command,
+                                                                                  chat_id: Chat.find_by(chat_id: chat).id).nickname} #{text}")
+          end
         end
       end
     end
