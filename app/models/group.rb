1e184fb0e47c581f15e1d793aa2ebb9ccad1edf4
Светлана Курилова <svetakurilova2001@gmail.com>
2022-06-28 13:02:17 +0300

    edit bot

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ApplicationRecord
   belongs_to :chat
-  validates :name, presence: true
-  validates :nickname, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true
+  validates :nickname, presence: true
 end
