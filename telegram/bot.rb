17ef43e17424eebab4a16c571c1a399a1c6deb8e
Светлана Курилова <svetakurilova2001@gmail.com>
2022-06-29 12:40:13 +0300

    code improvement

--- telegram/bot.rb
+++ telegram/bot.rb
@@ -8,22 +8,22 @@ token = ENV.fetch('TOKEN', nil)
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
-    chat = message.chat.id
+    chat_id = message.chat.id
 
     # rubocop:disable
     if message.is_a?(Telegram::Bot::Types::Message)
-    # rubocop:enable
-      command, text = message.text&.match(/\/(\S+)\s*(.*)/)&.captures
+      # rubocop:enable
+      command, text = message.text&.match(%r{/(\S+)\s*(.*)})&.captures
       case command
+      # rubocop:disable Style/HashSyntax
       when 'start'
-        bot.api.send_message(chat_id: message.chat.id,
-                            text:    "Привет, #{message.from.first_name}. Зарегестрируйся на сайте (localhost:3000). Твой id Телеграм чата #{message.chat.id}")
+        bot.api.send_message(chat_id: chat_id,
+                             text:    "Привет, #{message.from.first_name}. Зарегестрируйся на сайте (localhost:3000). Твой id Телеграм чата #{chat_id}")
       when command
-        if Group.find_by(name: command, chat_id: Chat.find_by(chat_id: chat)&.id)
-          bot.api.send_message(chat_id: message.chat.id, text: "#{Group.find_by(name:    command,
-                                                                                chat_id: Chat.find_by(chat_id: chat).id).nickname} #{text}")
-        end
+        group = Group.find_by(name: command, chat_id: Chat.find_by(chat_id:)&.id)
+        bot.api.send_message(chat_id: chat_id, text: "#{group.nickname} #{text}") if group
       end
+      # rubocop:enable Style/HashSyntax
     end
   end
 end
